d85d866e4bf69b6895c5713017158e20a2372f2e
super-continent <46540468+super-continent@users.noreply.github.com>
2022-05-24 16:44:15 -0700

    add AOB scanning support

--- src/game/offset.rs
+++ src/game/offset.rs
@@ -1,28 +1,8 @@
 #![allow(dead_code)]
+use crate::{pattern, helpers::{Offset, AobSignature}};
 
-pub const FN_LOAD_BBSCRIPT: Address = Address::offset(0xBBF000);
-pub const FN_LOOP_ROOT: Address = Address::offset(0x9CDF30); // Maybe the game loops outermost layer, runs once per frame
-pub const FN_WNDPROC: Address = Address::offset(0x94AF60);
+pub const FN_LOAD_BBSCRIPT: Offset = Offset::new(0xBBF000);
+pub const FN_LOOP_SLOWDOWN: Offset = Offset::new(0x9CDF30);
 
-pub enum Address<'a> {
-    Offset(u32),
-    Pattern(&'a[u8])
-}
-
-impl<'a> Address<'a> {
-    pub const fn offset(offset: u32) -> Self {
-        Self::Offset(offset)
-    }
-
-    pub unsafe fn get_address(&self) -> u32 {
-        match self {
-            Self::Offset(offset) => {
-                let base = winapi::um::libloaderapi::GetModuleHandleA(std::ptr::null_mut()) as u32;
-                base + offset
-            }
-            Self::Pattern(_pattern) => {
-                todo!()
-            }
-        }
-    }
-}
+// BattleState::ControlBattleObject(state *)
+pub const FN_CONTROL_BATTLE_OBJECT: Offset = Offset::new(0x9DE7F0);
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,10 +1,13 @@
-use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
+use std::{ffi::OsStr, mem};
 
+use winapi::um::memoryapi::ReadProcessMemory;
+use winapi::um::psapi::GetModuleInformation;
 use winapi::{
     ctypes::c_int,
     shared::{minwindef::*, windef::HWND},
     um::{
+        processthreadsapi::GetCurrentProcess,
         winnt::LONG,
         winuser::{
             CallWindowProcA, CallWindowProcW, GetWindowLongA, GetWindowLongW, IsWindowUnicode,
@@ -39,6 +42,10 @@ pub unsafe fn get_window_long(hwnd: HWND, n_index: INT) -> LONG {
     };
 }
 
+pub unsafe fn get_module_base() -> HINSTANCE {
+    winapi::um::libloaderapi::GetModuleHandleA(std::ptr::null_mut())
+}
+
 pub unsafe fn call_wndproc(
     prev_wnd_func: WNDPROC,
     hwnd: HWND,
@@ -58,4 +65,125 @@ pub fn win32_wstring(val: &str) -> Vec<u16> {
         .encode_wide()
         .chain(std::iter::once(0))
         .collect::<Vec<u16>>()
+}
+
+/// Create an [`Address`] that scans for a specified pattern.
+#[macro_export]
+macro_rules! pattern {
+    ( $( $x:tt ),* ) => {
+        crate::helpers::AobSignature::new(
+            &[ $( pattern!(@ONE_ELEMENT $x), )* ]
+        )
+    };
+    (@ONE_ELEMENT $x:literal) => { Some($x) };
+    (@ONE_ELEMENT _) => { None };
+}
+
+/// Type for storing offsets and AOB scans
+/// for finding the offset of something within a running program
+pub struct Offset(u32);
+
+impl Offset {
+    /// Create an [`Address`] that calculates the offset of a programs base address
+    pub const fn new(offset: u32) -> Self {
+        Self(offset)
+    }
+
+    pub unsafe fn get_address(&self) -> u32 {
+        let base = get_module_base() as u32;
+        base + self.0
+    }
+}
+
+pub struct AobSignature<'a>(&'a [Option<u8>]);
+
+impl<'a> AobSignature<'a> {
+    pub const fn new(signature: &'a [Option<u8>]) -> Self {
+        Self(signature)
+    }
+
+    pub unsafe fn find_in_base_module(&self) -> Option<u32> {
+        use winapi::um::psapi::MODULEINFO;
+
+        let process = GetCurrentProcess();
+        let module = get_module_base();
+
+        let mut module_info = mem::zeroed::<MODULEINFO>();
+
+        if GetModuleInformation(
+            process,
+            module,
+            &mut module_info,
+            mem::size_of::<MODULEINFO>() as u32,
+        ) == 0
+        {
+            return None;
+        };
+
+        let mut buffer = vec![0u8; module_info.SizeOfImage as usize];
+        let mut bytes_read = 0;
+
+        if ReadProcessMemory(
+            process,
+            module_info.lpBaseOfDll,
+            buffer.as_mut_ptr() as *mut _,
+            module_info.SizeOfImage as usize,
+            &mut bytes_read,
+        ) == 0
+        {
+            return None;
+        }
+        debug!("bytes read: {}", bytes_read);
+        debug!(
+            "Scanning AOB, address = {:X}, size = {:X}",
+            module as u32, module_info.SizeOfImage
+        );
+
+        debug!("patlen = {}, bufferlen = {}", self.0.len(), buffer.len());
+        if let Some(offset) = scan_aob(&buffer, self.0) {
+            Some(module as u32 + offset as u32)
+        } else {
+            None
+        }
+    }
+}
+
+// Boyer-Moore-Horspool substring search
+// Code taken and modified from hudhook (https://github.com/veeenu/hudhook)
+pub fn scan_aob(haystack: &[u8], needle: &[Option<u8>]) -> Option<usize> {
+    let (pattern_len, haystack_len) = (needle.len(), haystack.len());
+
+    if pattern_len > haystack_len {
+        debug!("pattern length greater than haystack length!");
+        panic!("pattern length must be less than haystack length")
+    }
+
+    let mut skips = [pattern_len; 256];
+    for pattern_idx in 0..pattern_len - 1 {
+        if let Some(v) = needle[pattern_idx] {
+            skips[v as usize] = pattern_len - pattern_idx - 1;
+        }
+    }
+
+    let mut haystack_idx = pattern_len - 1;
+    while haystack_idx < haystack_len {
+        let mut hay_idx = haystack_idx;
+        for pat_idx in (0..pattern_len).rev() {
+            if !(needle[pat_idx as usize].is_none()
+                || needle[pat_idx as usize] == Some(haystack[hay_idx as usize]))
+            {
+                break;
+            }
+
+            if pat_idx == 0 {
+                return Some(hay_idx);
+            }
+
+            hay_idx -= 1;
+        }
+
+        haystack_idx += skips[haystack[haystack_idx] as usize];
+    }
+
+    None
 }
