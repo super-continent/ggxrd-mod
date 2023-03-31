da647d6363567aba4ffab006761f085ec68df45b
super-continent <46540468+super-continent@users.noreply.github.com>
2023-03-31 16:59:09 -0700

    update offsets

--- src/game/offset.rs
+++ src/game/offset.rs
@@ -2,10 +2,10 @@
 use crate::{helpers::{Offset}};
 
 /// BBSAnalyzeExe
-pub const FN_LOAD_BBSCRIPT: Offset = Offset::new(0xBD5150);
+pub const FN_LOAD_BBSCRIPT: Offset = Offset::new(0xBD5250);
 /// BattleState::ControlBattleObject(state *)
-pub const FN_CONTROL_BATTLE_OBJECT: Offset = Offset::new(0x9E7370);
+pub const FN_CONTROL_BATTLE_OBJECT: Offset = Offset::new(0x9E6DB0);
 /// AREDGameInfo_Battle::UpdateBattle(state *)
-pub const FN_UPDATE_BATTLE: Offset = Offset::new(0xA5E7C0);
+pub const FN_UPDATE_BATTLE: Offset = Offset::new(0xA5E490);
 
 // pub const GAME_STATE: Offset = Offset::new(0x1987724);
